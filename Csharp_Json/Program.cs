0fa15d53a0a4fc3f233ac095611a3c93b7633bc6
Turgho <87340089+Turgho@users.noreply.github.com>
2023-05-28 16:33:31 -0300

    Add files via upload

---
+++ Csharp_Json/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Csharp_Json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/pikachu/");
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            string json = @"{ ""nome"" : ""Jose Carlos"", ""sobrenome"" : ""Macoratti"", ""email"": ""macoratti@yahoo.com"" }";
+
+            dynamic resultado = serializer.DeserializeObject(json);
+
+            foreach (KeyValuePair<string, object> entry in resultado)
+            {
+                var key = entry.Key;
+                var value = entry.Value as string;
+                Console.WriteLine(String.Format("{0} : {1}", key, value));
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(serializer.Serialize(resultado));
+            Console.WriteLine("");
+            Console.ReadKey();
+
+            List<string> Especies = new List<string> {
+                "charmander",
+                "squirtle",
+                "bulbasaur"
+            };
+
+            Console.WriteLine($"Escolha um dos pokemons abaixo:\n");
+            foreach (string pokemons in Especies)
+            {
+                Console.WriteLine(pokemons);
+            }
+            Console.Write("\nFaça sua escolha: ");
+
+            while (true)
+            {
+                string escolha = Console.ReadLine().ToLower();
+
+                if (Especies.Contains(escolha))
+                {
+                    Console.WriteLine($"\nVocê escolheu {escolha}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Pokemon indisponível!");
+                }
+            }
+        }
+    }
+}
---
+++ Desafio - Alura/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Desafio_Alura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/pikachu/");
+            var request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
+
+            dynamic resultado = serializer.DeserializeObject(response.Content);
+
+            foreach (KeyValuePair<string, object> entry in resultado)
+            {
+                var key = entry.Key;
+                var value = entry.Value as string;
+                Console.WriteLine(key, value);
+            }
+        }
+    }
+}
